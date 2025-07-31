a83d02dd58a895e09183875e76931a3be742b434
shem4soul@gmail.com <shem4soul@gmail.com>
2025-07-31 16:55:55 +0100

    Refactor middleware to handle favicon requests and improve logging

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
-const http = require('http');
-
-const express = require('express');
-
+const http = require("http");
+const express = require("express");
 const app = express();
 
-app.use((req, res, next) =>{
-    console.log('In the middleware');
-   next(); // Allows the request to the next middleware in line    
+app.use((req, res, next) => {
+  if (req.url === "/favicon.ico") return res.status(204).end(); // skip favicon requests
+  console.log("In the middleware", req.url);
+  next();
 });
 
-app.use((req, res, next) =>{
-    console.log('In another middleware');
-//...    
-})
+app.use((req, res) => {
+  console.log("In another middleware");
+  res.send("<h1>Hello from Express!</h1>");
+});
 
 const server = http.createServer(app);
-
-server.listen(3000)
+server.listen(3000, () => {
+  console.log("Server is listening on port 3000");
+});
