8394841e05df242d30e817b8c35c072c33d9d5ae
shem4soul@gmail.com <shem4soul@gmail.com>
2025-08-02 09:17:47 +0100

    Serve 404 error page as HTML file instead of inline text

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require("express");
 const app = express();
 
@@ -15,7 +16,7 @@ app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use((req, res, next) =>{
-   res.status(404).send('<h>Page not found</h>');
+   res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
 });
 
 app.listen(3000);
