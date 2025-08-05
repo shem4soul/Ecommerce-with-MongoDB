fcd08134b78c48727c79680ae9076ef35211d300
shem4soul@gmail.com <shem4soul@gmail.com>
2025-08-05 19:53:13 +0100

    updated controller

--- controllers/error.js
+++ controllers/error.js
@@ -1,3 +1,6 @@
-exports.get404 = (req, res, next) =>{
-   res.status(404).render('404', { pageTitle: 'Page Not Found'});
+exports.get404 = (req, res, next) => {
+  res.status(404).render("404", {
+    pageTitle: "Page Not Found",
+    path: "/404", // or just '' if you don't want it to match anything
+  });
 };
--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const errorController = require('./controllers/error');
 
 const app = express();
 
+
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
