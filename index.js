433b0cdd3718c95b4c4171bce39fb7bc417d71df
shem4soul@gmail.com <shem4soul@gmail.com>
2025-08-05 16:46:26 +0100

    upadted index.js

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const path = require('path');
 const express = require("express");
 
+const errorController = require('./controllers/error');
+
 const app = express();
 
 app.set('view engine', 'ejs');
@@ -21,7 +23,5 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
-app.use(
-});
-
+app.use(errorController.get404);
 app.listen(3000);
