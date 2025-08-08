87ec2cf7f1fad5c92b46e67fdd3e3d7cf2ea7f29
shem4soul@gmail.com <shem4soul@gmail.com>
2025-08-08 15:06:37 +0100

    updated product model

--- controllers/products.js
+++ controllers/products.js
@@ -2,7 +2,7 @@
 const Product = require('../models/product');
 
 const getAddProduct = (req, res, next) => {
-    res.render('add-product', {
+    res.render('admin/add-product', {
         pageTitle: 'Add Product',
         path: '/admin/add-product',
         formCSS: true,
@@ -19,7 +19,7 @@ product.save()
 
 const getProducts = (req, res, next) => {
     Product.fetchAll(products => {
-        res.render("shop", {
+        res.render("shop/product-list", {
           prods: products,
           pageTitle: "Shop",
           path: "/",
