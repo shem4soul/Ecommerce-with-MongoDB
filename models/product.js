0c6a22f80824f872c696238118f4fa20cab123de
shem4soul@gmail.com <shem4soul@gmail.com>
2025-08-08 14:08:04 +0100

    refactor product model

--- models/product.js
+++ models/product.js
@@ -1,6 +1,21 @@
 const fs = require('fs');
 const path = require('path');
 
+const getProductsFromFile = cb => {
+           const p = path.join(
+            path.dirname(process.mainModule.filename),
+            'data',
+            'products.json'
+        );
+        fs.readFile(p, (err, fileContent) => {
+            if (err) {
+                cb([]);
+            } else{
+            cb(JSON.parse(fileContent));
+            }
+        });
+      
+    };
 
 
 module.exports = class Product {
@@ -9,35 +24,17 @@ module.exports = class Product {
     }
   
     save() {
-       const p = path.join(path.dirname(process.mainModule.filename), 
-       'data', 
-       'products.json')
-       ;
+      getProductsFromFile();
        fs.readFile(p, (err, fileContent) => {
-        let products = [];
-        if (!err) {
-            products = JSON.parse(fileContent);
-        }
-        products.push(this);
+          products.push(this);
         fs.writeFile(p, JSON.stringify(products), (err) => {
             console.log(err);
-            
         })
        });
     }
    
     static fetchAll(cb) {
-        const p = path.join(
-            path.dirname(process.mainModule.filename),
-            'data',
-            'products.json'
-        );
-        fs.readFile(p, (err, fileContent) => {
-            if (err) {
-                cb([]);
-            }
-            cb(JSON.parse(fileContent))
-        })
+       getProductsFromFile(cb);
       
     }
-}
+};
