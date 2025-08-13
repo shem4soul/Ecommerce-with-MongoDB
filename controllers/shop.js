054d648f75978a059220a5f6a8255d6b3aec898d
shem4soul@gmail.com <shem4soul@gmail.com>
2025-08-13 21:06:57 +0100

    added new product

--- controllers/shop.js
+++ controllers/shop.js
@@ -29,7 +29,9 @@ exports.getCart = (req, res, next) => {
 
 exports.getProduct = (req, res, next) => {
   const prodId = req.params.productId;
-  console.log(prodId);
+  Product.findById(prodId, product => {
+    console.log(product);
+  })
    res.redirect('/');  
 };
 
