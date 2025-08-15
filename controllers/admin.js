afca6efde8cfeb363f5afac73dd5ab95efc06f82
shem4soul@gmail.com <shem4soul@gmail.com>
2025-08-15 13:49:19 +0100

    upated cart

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,13 +1,10 @@
 const Product = require("../models/product");
 
 exports.getAddProduct = (req, res, next) => {
-  res.render("admin/add-product", {
+  res.render("admin/edit-product", {
     pageTitle: "Add Product",
     path: "/admin/add-product",
-    formsCSS: true,
-    productCSS: true,
-    activeAddProduct: true,
-  });
+     });
 };
 
 exports.postAddProduct = (req, res, next) => {
