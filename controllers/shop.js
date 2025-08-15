295136caebb2bfc49ec806d1bc5c41ac2c35e5fa
shem4soul@gmail.com <shem4soul@gmail.com>
2025-08-15 12:33:33 +0100

    updated cart

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,4 +1,5 @@
 const Product = require("../models/product");
+const Cart = require('../models/cart');
 
 exports.getProducts = (req, res, next) => {
   Product.fetchAll((products) => {
@@ -29,10 +30,12 @@ exports.getCart = (req, res, next) => {
 
 exports.postCart = (req, res, next) => {
   const prodId = req.body.productId;
-  console.log(prodId);
+ Product.findById(prodId, (product) => {
+  Cart.addProduct(prodId, product.price);
+ });
   res.redirect('/cart');
   
-}
+};
 
 exports.getProduct = (req, res, next) => {
   const prodId = req.params.productId;
