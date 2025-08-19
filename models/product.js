794078aa798f736885ef6f658916d091a312c276
shem4soul@gmail.com <shem4soul@gmail.com>
2025-08-19 19:07:34 +0100

    updated the database schema

--- models/product.js
+++ models/product.js
@@ -13,12 +13,13 @@ module.exports = class Product {
   }
 
   save() {
-  
+    return db.execute(
+      'INSERT INTO products (title, price, imageUrl, description) VALUES (?, ?, ?, ?)',
+      [this.title, this.price, this.imageUrl, this.description]
+    );
   }
 
-  static deleteById(id) {
-    
-  }
+  static deleteById(id) {}
 
   static fetchAll(cb) {
    return db.execute('SELECT * FROM products');
