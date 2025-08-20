e1c305189a8b481bc76ab93335f460d21d542f9a
shem4soul@gmail.com <shem4soul@gmail.com>
2025-08-20 19:21:25 +0100

    updated the deatils

--- controllers/shop.js
+++ controllers/shop.js
@@ -2,10 +2,10 @@ const Product = require("../models/product");
 const Cart = require("../models/cart");
 
 exports.getProducts = (req, res, next) => {
-  Product.fetchAll() 
-    .then(([rows, fieldData]) => {
+  Product.findAll() 
+    .then(products => {
     res.render("shop/product-list", {
-      prods: rows,
+      prods: products,
       pageTitle: "All Products",
       path: "/products",
     });
@@ -27,17 +27,6 @@ exports.getProduct = (req, res, next) => {
 
 };
 
-// exports.getIndex = (req, res, next) => {
-//   Product.fetchAll()
-//    .then(([rows, fieldData]) => {
-//       res.render('shop/index', {
-//         prods: rows,
-//         pageTitle: 'Shop',
-//         path: '/'
-//       });
-//    })
-//    .catch(err => console.log(err));
-//   };
 
 exports.getIndex = (req, res, next) => {
   Product.findAll()
