363d5a443d785355d853f65ce2a1cc4c8870a9e3
shem4soul@gmail.com <shem4soul@gmail.com>
2025-08-20 19:28:17 +0100

    updated get product

--- controllers/shop.js
+++ controllers/shop.js
@@ -15,17 +15,20 @@ exports.getProducts = (req, res, next) => {
 
 exports.getProduct = (req, res, next) => {
   const prodId = req.params.productId;
-  Product.findById(prodId)
-   .then(([product]) => {
+  Product.findByPk(prodId)
+    .then((product) => {
+      if (!product) {
+        return res.redirect("/products"); // Or show 404
+      }
       res.render("shop/product-detail", {
-      product: product[0],
-      pageTitle: product.title,
-      path: "/products",
-    });
-   })
-   .catch(err => console.log(err));
-
+        product: product,
+        pageTitle: product.title,
+        path: "/products",
+      });
+    })
+    .catch((err) => console.log(err));
 };
+
 
 
 exports.getIndex = (req, res, next) => {
