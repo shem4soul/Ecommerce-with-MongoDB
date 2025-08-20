b88fd157c97f37dae4757216bd05b0dfdc2044cd
shem4soul@gmail.com <shem4soul@gmail.com>
2025-08-20 13:58:10 +0100

    updated admin

--- controllers/admin.js
+++ controllers/admin.js
@@ -20,7 +20,7 @@ exports.postAddProduct = (req, res, next) => {
     description: description
   })
   .then(result => {
-    console.log(result);
+    // console.log(result);
     console.log('Created Product');
       })
       .catch(err => {
