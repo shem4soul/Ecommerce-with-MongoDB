1d388adf11e694d8c49c78bc1ef671cac4b1b43d
shem4soul@gmail.com <shem4soul@gmail.com>
2025-08-20 12:20:22 +0100

    updated my database to sequelize

--- utils/database.js
+++ utils/database.js
@@ -1,11 +1,8 @@
-const mysql = require('mysql2');
+const Sequelize = require('sequelize');
 
-const pool = mysql.createPool({
+const sequelize = new Sequelize('node-complete', 'root', 'church', {
+    dialect: 'mysql',
     host: 'localhost',
-    user: 'root',
-    database: 'node-complete',
-    password: 'church'
 });
 
-
-module.exports = pool.promise();
+module.exports = sequelize;
