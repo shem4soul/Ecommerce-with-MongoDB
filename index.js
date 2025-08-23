ffa8a44c5f8f5fc8307dc1eedeed6c52614bf2a2
shem4soul@gmail.com <shem4soul@gmail.com>
2025-08-23 17:23:06 +0100

    Added one to many Relationship

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const bodyParser = require("body-parser");
 
 const errorController = require("./controllers/error");
 const sequelize = require('./utils/database')
+const Product = require('./models/product');
+const User = require('./models/user');
 
 const app = express();
 
@@ -22,6 +24,9 @@ app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
+
+Product.belongsTo(User, {constraints: true, oneDelete: 'CASCADE'});
+User.hasMany(Product);
 
 sequelize
   .sync()
