5a218a310a583cf76a9d190908f85faab0dd5fba
shem4soul@gmail.com <shem4soul@gmail.com>
2025-08-26 15:18:15 +0100

    updated the shop model

--- controllers/shop.js
+++ controllers/shop.js
@@ -71,25 +71,7 @@ exports.getCart = (req, res, next) => {
 
      })
      .catch(err => console.log(err));
-  // Cart.getCart((cart) => {
-  //   Product.fetchAll((products) => {
-  //     const cartProducts = [];
-  //     for (product of products) {
-  //       const cartProductData = cart.products.find(
-  //         (prod) => prod.id === product.id
-  //       );
-  //       if (cartProductData) {
-  //         cartProducts.push({ productData: product, qty: cartProductData.qty });
-  //       }
-      }
-//       res.render("shop/cart", {
-//         path: "/cart",
-//         pageTitle: "Your Cart",
-//         products: cartProducts,
-//       });
-//     });
-//   });
-// };
+       }
 
 exports.postCart = (req, res, next) => {
   const prodId = req.body.productId;
