395cfe0571a5999aa5a15df92145f131ab268d5d
shem4soul@gmail.com <shem4soul@gmail.com>
2025-09-02 20:18:49 +0100

    feat: implement mongoDB connection logic in database utility

--- utils/database.js
+++ utils/database.js
@@ -1,1 +1,19 @@
-const mongodb
+const mongodb = require('mongodb');
+
+const MongoClient = mongodb.MongoClient;
+
+
+const mongoConnect = callback => {
+    MongoClient.connect(
+      "mongodb+srv://smartresearchers82:church@nodejsexpressprojects.caj9s.mongodb.net/?retryWrites=true&w=majority&appName=NodejsExpressProjects"
+    )
+      .then((result) => {
+        console.log("Connected");
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+
+}
+
+module.exports = mongoConnect;
