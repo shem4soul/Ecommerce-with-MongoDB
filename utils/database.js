e954cd6d4a5870cc012c32084e147fb6fe719aa2
shem4soul@gmail.com <shem4soul@gmail.com>
2025-09-07 13:20:54 +0100

    fix: correct database connection logic and improve error handling

--- utils/database.js
+++ utils/database.js
@@ -2,18 +2,30 @@ const mongodb = require('mongodb');
 
 const MongoClient = mongodb.MongoClient;
 
+let _db;
 
 const mongoConnect = callback => {
     MongoClient.connect(
-      "mongodb+srv://smartresearchers82:church@nodejsexpressprojects.caj9s.mongodb.net/?retryWrites=true&w=majority&appName=NodejsExpressProjects"
+      "mongodb+srv://smartresearchers82:church@nodejsexpressprojects.caj9s.mongodb.net/shop?retryWrites=true&w=majority&appName=NodejsExpressProjects"
     )
       .then((result) => {
         console.log("Connected");
+        _db = client.db();
+        callback();
       })
       .catch((err) => {
         console.log(err);
+        throw err;
       });
 
 }
 
+const getDb = () => {
+   if (_db) {
+    return _db;
+   }
+   throw 'No database found!';
+}
+
 module.exports = mongoConnect;
+module.exports = getDb;
