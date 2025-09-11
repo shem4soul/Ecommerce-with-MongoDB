5e30c84290285e5e2635cc50b628b4f8d3387dc2
shem4soul@gmail.com <shem4soul@gmail.com>
2025-09-11 17:20:57 +0100

    fix: enhance save and fetchAll methods with proper promise handling

--- models/product.js
+++ models/product.js
@@ -10,7 +10,28 @@ class Product {
 
   save() {
     const db = getDb();
-    return db.collection("products").insertOne(this);
+    return db.collection("products")
+    .insertOne(this)
+    .then(result => {
+      console.log(result);
+      })
+    .catch(err => {
+    console.log(err);
+    })
+  }
+
+  static fetchAll() {
+    return db
+      .collection('products')
+      .find()
+      .toArray()
+      .then(products => {
+        console.log(products);
+        return products;
+      })
+      .catch(err => {
+        console.log(err);
+      })
   }
 }
 
