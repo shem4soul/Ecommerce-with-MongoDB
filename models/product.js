89d51dd2f354fdf0203a5f91a9c547b60f771780
shem4soul@gmail.com <shem4soul@gmail.com>
2025-09-14 21:17:22 +0100

    fix: add id parameter to Product constructor to support product identification

--- models/product.js
+++ models/product.js
@@ -2,11 +2,12 @@ const mongodb = require('mongodb');
 const getDb = require("../utils/database").getDb;
 
 class Product {
-  constructor(title, price, description, imageUrl) {
+  constructor(title, price, description, imageUrl, id) {
     this.title = title;
     this.price = price;
     this.description = description;
     this.imageUrl = imageUrl;
+    this._id = id;
   }
 
   save() {
