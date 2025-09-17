b855ceef2440f519ef3fec59dcb9b46c7121fcb7
shem4soul@gmail.com <shem4soul@gmail.com>
2025-09-17 18:03:44 +0100

    updated the index.js

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use((req, res, next) => {
   User.findById('68c9abd22d3b487d3f0cc3a0')
     .then((user) => {
-      req.user = user;
+      req.user = new User(user.name, user.email, user.cart, user._id);
       next();
     })
     .catch((err) => console.log(err));
