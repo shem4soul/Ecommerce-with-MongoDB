775b30a3fda9cfde077f334b30d0a7552565ea70
shem4soul@gmail.com <shem4soul@gmail.com>
2025-09-19 16:57:37 +0100

    added get orders funtionality

--- models/user.js
+++ models/user.js
@@ -79,26 +79,46 @@ class User {
 
    addOrder() {
      const db = getDb();
-     return db
-      .collection('orders')
-      .insertOne(this.cart)
-      .then(result => {
-        this.cart = {items: []};        
-        return db
-        .collection('users')
-        .updateOne(     
+    return  this.getCart()
+     .then(products => {
+       const order = {      
+        items: products,
+        user: {
+            _id: new ObjectId(this._id),    
+            name: this.name
+        }
+       };
+       return db.collection('orders')
+       .insertOne(order);
+     })
+     .then(result => {
+       this.cart = {items: []};
+       return db
+       .collection('users')
+       .updateOne(     
         {_id: new ObjectId(this._id)},      
         {$set: {cart: {items: []}}}
-        );
-      });
-    }                                                            
-      
-  
+       );
+     });
+     
+    }   
+    
+
+    getOrders() {
+     const db = getDb();
+     return db
+     .collection('orders')
+     .find({'user._id': new ObjectId(this._id)})
+     .toArray();
+    }
+    
+
     static findById(userId) {
      const db = getDb();
      return db.collection('users')
      .findOne({_id: new ObjectId(userId)})
-     .then(user => {
+     .then(user =>
+         {
          console.log(user);
          return user;
      })
