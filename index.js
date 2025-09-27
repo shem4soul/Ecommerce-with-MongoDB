29dbfd622779e60fce602399deebb59f213a491a
shem4soul@gmail.com <shem4soul@gmail.com>
2025-09-27 21:03:05 +0100

    refactor: uncomment admin routes and comment out unused user model

--- index.js
+++ index.js
@@ -8,30 +8,30 @@ const mongoose = require('mongoose');
 
 const errorController = require("./controllers/error");
 
-const User = require('./models/user');
+// const User = require('./models/user');
 
 const app = express();
 
 app.set("view engine", "ejs");
 app.set("views", "views");
 
-// const adminRoutes = require("./routes/admin");
+const adminRoutes = require("./routes/admin");
 // const shopRoutes = require("./routes/shop");
 
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use((req, res, next) => {
-  User.findById('68c9abd22d3b487d3f0cc3a0')
-    .then((user) => {
-      req.user = new User(user.name, user.email, user.cart, user._id);
-      next();
-    })
-    .catch((err) => console.log(err));
-});
+// app.use((req, res, next) => {
+//   User.findById('68c9abd22d3b487d3f0cc3a0')
+//     .then((user) => {
+//       req.user = new User(user.name, user.email, user.cart, user._id);
+//       next();
+//     })
+//     .catch((err) => console.log(err));
+// });
 
-// app.use("/admin", adminRoutes);
+app.use("/admin", adminRoutes);
 // app.use(shopRoutes);
  
 app.use(errorController.get404);
