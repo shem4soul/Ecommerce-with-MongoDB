e95db4c9d2a38d624e0ba3635d98ca05b8f75b3d
shem4soul@gmail.com <shem4soul@gmail.com>
2025-09-29 20:11:26 +0100

    fix: implement addToCart method to handle product quantity updates in user cart

--- models/user.js
+++ models/user.js
@@ -17,9 +17,36 @@ const userSchema = new Schema({
         required: true
     },
     cart: {
-        items: [{productId: {type: Schema.Types.ObjectId, ref: 'product', required: true}, quantity: {type: Number, required: true,}}]
+        items: [{
+            productId: {type: Schema.Types.ObjectId, ref: 'product', required: true}, 
+            quantity: {type: Number, required: true,}}]
     }
 });
+
+userSchema.methods.addToCart = function(product) {
+    const cartProductIndex = this.cart.items.findIndex(cp => {
+   return cp.productId.toString() === product._id.toString();
+            });
+            let newQuantity = 1;
+            const updatedCartItems = [...this.cart.items];
+
+
+            if (cartProduct >= 0) {
+                newQuantity = this.cart.items[cartProduct].quantity + 1;
+                updatedCartItems[cartProduct].quantity = newQuantity;
+            }   else { 
+                updatedCartItems.push({
+             productId: product._id,
+              quantity: newQuantity
+             });
+            }     
+            const updatedCart  = {
+                items: updatedCartItems
+            };
+            this.cart = updatedCart;    
+            return this.save();
+    
+};
 
 module.exports = mongoose.model('User', userSchema);
 
@@ -42,31 +69,7 @@ module.exports = mongoose.model('User', userSchema);
 //         }
   
 //         addToCart(product) {
-//             const cartProduct = this.cart.items.findIndex(cp => {
-//             return cp.productId.toString() === product._id.toString();
-//             });
-//             let newQuantity = 1;
-//             const updatedCartItems = [...this.cart.items];
-
-
-//             if (cartProduct >= 0) {
-//                 newQuantity = this.cart.items[cartProduct].quantity + 1;
-//                 updatedCartItems[cartProduct].quantity = newQuantity;
-//             }   else { 
-//                 updatedCartItems.push({
-//              productId: new ObjectId(product._id),
-//               quantity: newQuantity
-//              });
-//             }     
-//             const updatedCart  = {
-//                 items: updatedCartItems
-//             };
-//             const db = getDb();
-//             return db.collection('users')
-//             .updateOne(
-//             {_id: new ObjectId(this._id)},
-//             {$set: {cart: updatedCart}}
-//             );
+//        
 //         }
 
 //     getCart() {
