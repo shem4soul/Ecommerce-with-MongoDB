ca3e2360779f4c64e2f88f44a1f5d47d9eb4b924
shem4soul@gmail.com <shem4soul@gmail.com>
2025-09-29 19:06:00 +0100

    fix: add userId field to product schema for user association

--- models/product.js
+++ models/product.js
@@ -17,6 +17,11 @@ const Schema = mongoose.Schema;
     imageUrl: {
       type: String,
       required: true
+    },
+    userId: {
+      type: Schema.Types.ObjectId,
+      ref: 'User',
+      required: true
     }
  })
 
