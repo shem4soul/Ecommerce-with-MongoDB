ccb568b365ef635cfc1d7b18f1d6639a2f40a7b2
shem4soul@gmail.com <shem4soul@gmail.com>
2025-09-29 18:53:37 +0100

    fix: add missing ref to productId in user cart schema

--- models/user.js
+++ models/user.js
@@ -17,7 +17,7 @@ const userSchema = new Schema({
         required: true
     },
     cart: {
-        items: [{productId: {type: Schema.Types.ObjectId, required: true}, quantity: {type: Number, required: true,}}]
+        items: [{productId: {type: Schema.Types.ObjectId, ref: 'product', required: true}, quantity: {type: Number, required: true,}}]
     }
 });
 
