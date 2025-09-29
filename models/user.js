c0057a0f4f195fadb6d88905195bd3731791ceb7
shem4soul@gmail.com <shem4soul@gmail.com>
2025-09-29 20:17:17 +0100

    fix: correct cart product index reference in addToCart method

--- models/user.js
+++ models/user.js
@@ -33,7 +33,7 @@ userSchema.methods.addToCart = function(product) {
 
             if (cartProductIndex >= 0) {
                 newQuantity = this.cart.items[cartProductIndex].quantity + 1;
-                updatedCartItems[cartProduct].quantity = newQuantity;
+                updatedCartItems[cartProductIndex].quantity = newQuantity;
             }   else { 
                 updatedCartItems.push({
              productId: product._id,
