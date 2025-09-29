a337e05d3845a7f0de5d2536d57103d066420ddf
shem4soul@gmail.com <shem4soul@gmail.com>
2025-09-29 20:37:15 +0100

    fix: remove execPopulate from getCart method and ensure correct product reference in user schema

--- controllers/shop.js
+++ controllers/shop.js
@@ -45,7 +45,6 @@ exports.getIndex = (req, res, next) => {
 exports.getCart = (req, res, next) => {
   req.user
   .populate('cart.items.productId')
-  .execPopulate()
     .then(user  => {
       const products = user.cart.items;
        res.render("shop/cart", {
--- models/user.js
+++ models/user.js
@@ -18,7 +18,7 @@ const userSchema = new Schema({
     },
     cart: {
         items: [{
-            productId: {type: Schema.Types.ObjectId, ref: 'product', required: true}, 
+            productId: {type: Schema.Types.ObjectId, ref: 'Product', required: true}, 
             quantity: {type: Number, required: true,}}]
     }
 });
