731fcf0c06d73e57035612cd5bb2644762d649d8
shem4soul@gmail.com <shem4soul@gmail.com>
2025-10-01 18:26:12 +0100

    fix: remove unused product import and ensure proper module export in order schema

--- models/order.js
+++ models/order.js
@@ -1,5 +1,4 @@
 const mongoose = require("mongoose");
-const product = require("./product");
 const Schema = mongoose.Schema;
 
 const orderSchema = new Schema({
@@ -21,3 +20,6 @@ const orderSchema = new Schema({
     }
     }
 });
+
+
+module.exports = mongoose.model('Order', orderSchema);
