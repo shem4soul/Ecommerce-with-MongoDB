6d5185257b2a672659edbcc4fbfa78c385d9a62a
shem4soul@gmail.com <shem4soul@gmail.com>
2025-10-01 18:44:29 +0100

    feat: implement order creation functionality in postOrder method

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,4 +1,5 @@
 const Product = require("../models/product");
+const Order = require('../models/order')
 
 exports.getProducts = (req, res, next) => {
   Product.find()
@@ -82,12 +83,25 @@ exports.postCartDeleteProduct = (req, res, next) => {
 
 exports.postOrder = (req, res, next) => {
    req.user
-    .addOrder()
-    .then((result) => {
-      res.redirect("/orders");
-    })
-    .catch((err) => console.log(err));  
-};  
+     .populate('cart.items.productId')
+       .then(user => {
+         const products = user.cart.items.map(i => {
+            return {quantity: i.quantity, product: i.productId};
+         });
+         const order = new Order({
+          user: {
+            name: req.user.name,
+            userId: req.user
+          },
+          products: products
+         });
+         return order.save();
+})
+.then(result => {
+  res.redirect('/orders');
+})
+.catch(err => console.log(err));
+};
 
 
 exports.getOrders = (req, res, next) => {
