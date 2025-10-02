40fc9f82217a53b57a8027ddfb5386387cef61fe
shem4soul@gmail.com <shem4soul@gmail.com>
2025-10-02 11:52:29 +0100

    updated error.js

--- controllers/error.js
+++ controllers/error.js
@@ -2,5 +2,6 @@ exports.get404 = (req, res, next) => {
   res.status(404).render("404", {
     pageTitle: "Page Not Found",
     path: "/404", // or just '' if you don't want it to match anything
+    isAuthenticated: req.isLoggedIn,
   });
 };
