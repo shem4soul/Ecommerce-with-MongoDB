8c51e4518049e1dafb665b377e0f1ea3b2743bbb
shem4soul@gmail.com <shem4soul@gmail.com>
2025-10-02 17:17:58 +0100

    refactor the cookies

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,4 +1,9 @@
 exports.getLogin = (req, res, next) => {
+    const isLoggedIn = req
+    .get('Cookie')
+    .split(';')[1]
+    .trim()
+    .split('=')[1];
     res.render("auth/login", {
       path: "/login",
       pageTitle: "Login",
