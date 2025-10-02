f512163a2bc4e774ceb9d05eedeecbcb0657ce3c
shem4soul@gmail.com <shem4soul@gmail.com>
2025-10-02 11:51:32 +0100

    fix a bug

--- controllers/admin.js
+++ controllers/admin.js
@@ -8,6 +8,7 @@ exports.getAddProduct = (req, res, next) => {
     pageTitle: "Add Product",
     path: "/admin/add-product",
     editing: false,
+    isAuthenticated: req.isLoggedIn,
   });
 };
 
@@ -51,7 +52,9 @@ exports.getEditProduct = (req, res, next) => {
         pageTitle: "Edit Product",
         path: "/admin/edit-product",
         editing: editMode,
-        product: product
+        product: product,
+        isAuthenticated: req.isLoggedIn
+
       });
     })
     .catch((err) => console.log(err));
