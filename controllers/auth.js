0383932c1ee05141a9912014c1dbb8fdd8b6c22e
shem4soul@gmail.com <shem4soul@gmail.com>
2025-10-09 10:33:20 +0100

    fix a bug

--- controllers/auth.js
+++ controllers/auth.js
@@ -29,31 +29,3 @@ exports.postLogout = (req, res, next) => {
 };
 
 
-// exports.getLogin = (req, res, next) => {
-//     // const isLoggedIn = req
-//     // .get('Cookie')
-//     // .split(';')[1]
-//     // .trim()
-//     // .split('=')[1] === 'true';
-//     console.log(req.session.isLoggedIn);
-//     res.render("auth/login", {
-//       path: "/login",
-//       pageTitle: "Login",
-//       isAuthenticated: false,
-//     });
-
-// };
-
-// exports.postLogin = (req, res, next) => {
-//   req.session.isLoggedIn = true;
-//     res.redirect('/');
-// };
-
-
-
-// exports.postLogout = (req, res, next) => {
-//   req.session.destroy((err) => {
-//     console.log(err);
-//     res.redirect("/");
-//   });
-// };
