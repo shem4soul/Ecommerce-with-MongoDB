fe91f63d78c2734f4def568e15830cb577ba739c
shem4soul@gmail.com <shem4soul@gmail.com>
2025-10-09 10:41:21 +0100

    added signup controller

--- controllers/auth.js
+++ controllers/auth.js
@@ -8,6 +8,14 @@ exports.getLogin = (req, res, next) => {
   });
 };
 
+exports.getSignup = (req, res, next) => {
+  res.render('auth/signup', {
+    path: 'signup',
+    pageTitle: 'Signup',
+    isAuthenticated: false
+  })
+}
+
 exports.postLogin = (req, res, next) => {
   User.findById("68d93ca53fdab82f4aeea625")
     .then((user) => {
@@ -20,6 +28,8 @@ exports.postLogin = (req, res, next) => {
     })
     .catch((err) => console.log(err));
 };
+
+exports.postSignup = (req, res, next) => {};
 
 exports.postLogout = (req, res, next) => {
   req.session.destroy(err => {
