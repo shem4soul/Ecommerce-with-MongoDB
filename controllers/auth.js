08e3f5a8119bc52d5acbf3ff2370aa5be468c965
shem4soul@gmail.com <shem4soul@gmail.com>
2025-10-09 12:07:11 +0100

    fix a bug

--- controllers/auth.js
+++ controllers/auth.js
@@ -19,13 +19,25 @@ exports.getSignup = (req, res, next) => {
 }
 
 exports.postLogin = (req, res, next) => {
-  User.findById("68d93ca53fdab82f4aeea625")
+    const email = req.body.email;
+    const password = req.body.password;
+    User.findOne({email: email})
     .then((user) => {
-      req.session.user = user;
+      if (!user) {
+        return res.redirect('/login');
+      }
+      bcrypt
+      .compare(password, user.password)
+      .then(doMatch => {
+        if (doMatch) {
       req.session.isLoggedIn = true;
-      req.session.save((err) => {
+       req.session.user = user;
+      return req.session.save((err) => {
         console.log(err);
         res.redirect("/");
+        });
+      }
+     res.redirect('login');
       });
     })
     .catch((err) => console.log(err));
