8ea25f3026066fcf3b3cd2c0f582caa06b6491a7
shem4soul@gmail.com <shem4soul@gmail.com>
2025-10-12 03:33:01 +0100

    remove its authenticated

--- controllers/admin.js
+++ controllers/admin.js
@@ -11,7 +11,7 @@ exports.getAddProduct = (req, res, next) => {
     pageTitle: "Add Product",
     path: "/admin/add-product",
     editing: false,
-    isAuthenticated: req.isLoggedIn,
+ 
   });
 };
 
@@ -56,9 +56,7 @@ exports.getEditProduct = (req, res, next) => {
         path: "/admin/edit-product",
         editing: editMode,
         product: product,
-        isAuthenticated: req.isLoggedIn
-
-      });
+            });
     })
     .catch((err) => console.log(err));
  
@@ -96,8 +94,7 @@ exports.getProducts = (req, res, next) => {
       prods: products,
       pageTitle: "Admin Products",
       path: "/admin/products",
-      isAuthenticated: req.isLoggedIn
-    });
+       });
   })
   .catch(err => console.log(err));
 };
