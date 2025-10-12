3ed710747378eb4651ea5265b003f6c23f9442c6
shem4soul@gmail.com <shem4soul@gmail.com>
2025-10-12 03:34:58 +0100

    remove its authenticated from shopjs

--- controllers/shop.js
+++ controllers/shop.js
@@ -9,8 +9,7 @@ exports.getProducts = (req, res, next) => {
         prods: products,
         pageTitle: "All Products",
         path: "/products",
-        isAuthenticated: req.session.isLoggedIn,
-      });
+        });
     })
     .catch((err) => {
       console.log(err);
@@ -25,8 +24,7 @@ exports.getProduct = (req, res, next) => {
         product: product,
         pageTitle: product.title,
         path: "/products",
-        isAuthenticated: req.session.isLoggedIn,
-      });
+         });
     })
     .catch((err) => console.log(err));
 };
@@ -55,8 +53,7 @@ exports.getCart = (req, res, next) => {
         path: "/cart",
         pageTitle: "Your Cart",
         products: products,
-        isAuthenticated: req.session.isLoggedIn,
-      });
+             });
     })
     .catch((err) => console.log(err));
 };
@@ -115,8 +112,7 @@ exports.getOrders = (req, res, next) => {
         path: "/orders",
         pageTitle: "Your Orders",
         orders: orders,
-        isAuthenticated: req.session.isLoggedIn,
-      });
+         });
     })
     .catch((err) => console.log(err));
 };
