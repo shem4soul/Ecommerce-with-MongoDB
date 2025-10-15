cf3fe755a341aebcaf53b001bc4c10d1812f0cc5
shem4soul@gmail.com <shem4soul@gmail.com>
2025-10-15 16:30:17 +0100

    fix  a bug

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,7 +3,8 @@ const bcrypt = require("bcryptjs");
 
 const User = require("../models/user");
 const transporter = require("../config/mailer");
-const { validatorResult } = require('express-validator');
+const { validationResult } = require("express-validator");
+
 
 exports.getLogin = (req, res, next) => {
   let message = req.flash("error");
@@ -70,13 +71,13 @@ exports.postSignup = (req, res, next) => {
   const email = req.body.email;
   const password = req.body.password;
   const confirmPassword = req.body.confirmPassword;
-  const errors = validateResult(req);
+  const errors = validationResult(req);
   if (!errors.isEmpty()) {
     console.log(errors.array());
     return res.status(422).render('auth/signup', {
       path: '/signup',
       pageTitle: 'Signup',
-      errorMessage: errors.array()
+      errorMessage: errors.array()[0].msg
     });
   }
 
