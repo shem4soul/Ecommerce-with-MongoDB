ed4f113e4aaca15f06fc318683c5bc29f5e8fed7
shem4soul@gmail.com <shem4soul@gmail.com>
2025-10-15 16:01:03 +0100

    added express validator

--- controllers/admin.js
+++ controllers/admin.js
@@ -71,21 +71,24 @@ exports.postEditProduct = (req, res, next) => {
   
   Product.findById(prodId)
    .then(product => {
+      if (product.userId.toString() !== req.user._id.toString()) {
+        return res.redirect('/');
+      }
     product.title = updatedTitle;
     product.price = updatedPrice;
     product.description = updatedDesc;
     product.imageUrl = updatedImageUrl;   
-    return product.save();
+    return product.save().then((result) => {
+      console.log("UPDATED PRODUCT!");
+      res.redirect("/admin/products");
+    });
    })
-      .then(result => {
-        console.log('UPDATED PRODUCT!');
-        res.redirect('/admin/products');
-         })
+      
   .catch(err => console.log(err));
 };
 
 exports.getProducts = (req, res, next) => {
-   Product.find()
+   Product.find({userId: req.user._id})
    //.select('title price -id')
   //  .populate('userId', 'name')
     .then(products => {
@@ -102,7 +105,7 @@ exports.getProducts = (req, res, next) => {
 
 exports.postDeleteProduct = (req, res, next) => {
   const prodId = req.body.productId;
-  Product.findByIdAndDelete(prodId)
+  Product.deleteOne({_id: prodId, userId: req.user._id})
   .then(() => {
     console.log('DESTROYED PRODUCT');
     res.redirect('/admin/products');
