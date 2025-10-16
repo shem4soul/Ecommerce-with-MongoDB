6fe77f06fb6e9973ed2a54691071cc42cee738e7
shem4soul@gmail.com <shem4soul@gmail.com>
2025-10-16 14:18:55 +0100

    fix bug

--- routes/admin.js
+++ routes/admin.js
@@ -4,8 +4,15 @@ const express = require("express");
 
 const adminController = require("../controllers/admin");
 const isAuth = require('../middleware/is-auth');
+const {
+  addProductValidation,
+  editProductValidation,
+} = require("../validators/admin");
 
 const router = express.Router();
+
+
+
 
 // // /admin/add-product => GET
 router.get("/add-product", isAuth, adminController.getAddProduct);
@@ -14,11 +21,11 @@ router.get("/add-product", isAuth, adminController.getAddProduct);
 router.get("/products", isAuth, adminController.getProducts);
 
 // // /admin/add-product => POST
-router.post("/add-product", isAuth, adminController.postAddProduct);
+router.post("/add-product", addProductValidation, isAuth, adminController.postAddProduct);
 
 router.get("/edit-product/:productId", isAuth, adminController.getEditProduct);
 
-router.post("/edit-product", isAuth, adminController.postEditProduct);
+router.post("/edit-product", editProductValidation, isAuth, adminController.postEditProduct);
 
 router.post("/delete-product", isAuth, adminController.postDeleteProduct);
 
---
+++ validators/admin.js
@@ -0,0 +1,30 @@
+
+const { body } = require("express-validator");
+
+exports.addProductValidation = [
+  body("title")
+    .isString()
+    .isLength({ min: 3 })
+    .trim()
+    .withMessage("Title must be at least 3 characters long."),
+  body("imageUrl").isURL().withMessage("Please enter a valid URL."),
+  body("price").isFloat().withMessage("Price must be a number."),
+  body("description")
+    .isLength({ min: 5, max: 400 })
+    .trim()
+    .withMessage("Description must be 5-400 characters long."),
+];
+
+exports.editProductValidation = [
+  body("title")
+    .isString()
+    .isLength({ min: 3 })
+    .trim()
+    .withMessage("Title must be at least 3 characters long."),
+  body("imageUrl").isURL().withMessage("Please enter a valid URL."),
+  body("price").isFloat().withMessage("Price must be a number."),
+  body("description")
+    .isLength({ min: 5, max: 400 })
+    .trim()
+    .withMessage("Description must be 5-400 characters long."),
+];
