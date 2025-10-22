8a8acffab431c47c3fd1d0ccaddc5dbac6693516
shem4soul@gmail.com <shem4soul@gmail.com>
2025-10-22 20:32:11 +0100

    added file picker to the ejs

--- index.js
+++ index.js
@@ -45,45 +45,47 @@ app.use(csrfProtection);
 app.use(flash());
 
 app.use((req, res, next) => {
-   if (!req.session.user) {
-    return next();
-   }
-  User.findById(req.session.user._id)
-    .then((user) => {
-     if(!user) {
-      return next();
-    }
-    req.user = user;
-    next();
-  })
-    .catch(err => {
-      throw new Error(err);
-    });    
-});
-
-app.use((req, res, next) => {
   res.locals.isAuthenticated = req.session.isLoggedIn;
   res.locals.csrfToken = req.csrfToken();
   next();
 });
 
-// app.use((req, res, next) => {
-//   res.locals.isAuthenticated = false; // set true to simulate logged-in
-//   next();
-// });
+app.use((req, res, next) => {
+  // throw new Error('Sync Dummy');
+  if (!req.session.user) {
+    return next();
+  }
+  User.findById(req.session.user._id)
+    .then((user) => {
+      if (!user) {
+        return next();
+      }
+      req.user = user;
+      next();
+    })
+    .catch((err) => {
+      next(new Error(err));
+    });
+});
 
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 app.use(authRoutes);
 
-
 app.get("/500", errorController.get500);
+
 app.use(errorController.get404);
 
 app.use((error, req, res, next) => {
-  //res.status(error.httpStatusCode).render(...);
-  res.redirect('/500');
+  // res.status(error.httpStatusCode).render(...);
+  // res.redirect('/500');
+  res.status(500).render("500", {
+    pageTitle: "Error!",
+    path: "/500",
+    isAuthenticated: req.session.isLoggedIn,
+  });
 });
+
 
 const PORT = process.env.PORT || 6000;
 const MONGO_URI = process.env.MONGO_URI;
