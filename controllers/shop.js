7149808c6fa3f38ca1a64b410f19324387739662
Shittu Emmanuel <shem4soul@gmail.com>
2025-10-28 16:32:16 +0100

    remove unnecessary execPopulate call in postOrder method

--- controllers/shop.js
+++ controllers/shop.js
@@ -166,7 +166,6 @@ exports.getCheckout = (req, res, next) => {
 exports.postOrder = (req, res, next) => {
   req.user
     .populate("cart.items.productId")
-    .execPopulate()
     .then((user) => {
       const products = user.cart.items.map((i) => {
         return { quantity: i.quantity, product: { ...i.productId._doc } };
